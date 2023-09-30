244c8a8743bd061d00fac7e883c36160e0844fcf
GUETDJL <1032205558@qq.com>
2023-09-30 14:42:30 +0800

    一维数组静态链表.cpp

--- 一维数组静态链表.cpp
+++ 一维数组静态链表.cpp
@@ -1,1 +1,30 @@
 # include<bits/stdc++.h>
+int nodes[150];
+int main()
+{
+    int n,m;
+    scanf("%d,%d",&n,&m);
+    for(int i = 1;i<=n-1;i++)
+    {
+        nodes[i]=i+1;
+    }
+    nodes[n]=1;
+    int now =1;prev=1;
+    while ((n--)>1)
+    {
+        for(int i=1;i<m;i++)
+        {
+            prev = now;
+            now = nodes[i];//到下个一节点
+
+        }
+        printf("%d",now);
+        nodes[prev] = nodes[now];//跳过节点，删除now
+        now =nodes[prev];//新的now节点
+
+
+        /* code */
+    }
+    printf("%d",now);
+    return 0 ;
+}
